a5d83a07842ac8c88bb2b9739e2b58fb36e7add6
Ruslebiff <joakivb@stud.ntnu.no>
2020-05-29 09:48:05 +0200

    added a menu to index.php

--- oppgave1-3/www/index.php
+++ oppgave1-3/www/index.php
@@ -1,2 +1,9 @@
 <?php
+require_once './vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array());
+
+echo $twig->render('index.html', array());
+
 echo phpinfo();
